c4ea61c651603c01989c1d41195c603c782e022c
gftea <ckq614@gmail.com>
2022-08-05 12:25:26 +0200

    move sender to Task type, remove Resource type

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use std::{
     future::Future,
     mem::forget,
     pin::Pin,
-    sync::{mpsc, Arc, Mutex},
-    task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
+    sync::{mpsc::{self}, Arc, Mutex},
+    task::{Context, Poll, RawWaker, RawWakerVTable, Waker}, time::Duration,
 };
 
 // export
@@ -40,25 +40,30 @@ impl Runtime {
             let task = Arc::new(Task {
                 fut: Mutex::new(Some(f)),
                 id: i,
+                sender: sender.clone(),
             });
             // to trigger first poll
             sender.send(task).unwrap();
         }
+        // drop the sender, and only have Task hold a sender clone, 
+        // once all tasks finished, all sender are dropped, so the receiver will know we are done
+        drop(sender);
 
         // --- Start Executor ---
         loop {
             // when receiver returns, it means the task is ready to advance further
-            let t = receiver.recv().unwrap();
+            let t = match receiver.recv() {
+                Ok(t) => t,
+                Err(_) => break,
+                
+            };
             // --- create context for future ---
             // data for Waker will have both channel sender and the task
             // so that in `wake` call, we can send the task back to Executor for polling
             // Data has to be Arc so that as long as at least one waker exist, the data will be alive, so that the task and sender
-            let data = Arc::new(Resource {
-                sender: sender.clone(),
-                task: t.clone(),
-            });
+            
             let rawwaker = RawWaker::new(
-                Arc::into_raw(data) as *const (),
+                Arc::into_raw(t.clone()) as *const (),
                 &RawWakerVTable::new(clone_rw, wake_rw, wake_by_ref_rw, drop_rw),
             );
             let waker = unsafe { Waker::from_raw(rawwaker) };
@@ -84,27 +89,26 @@ impl Runtime {
             if let Poll::Ready(_) = res {
                 println!("Task {} completed", t.id);
                 continue;
+                
             } else {
                 // store the future back
                 *mtx = Some(f);
                 println!("Task {} is pending, waiting for next task.", t.id);
             }
-
             
         }
+        println!("Exit runtime!");
     }
 }
-struct Resource {
-    sender: mpsc::Sender<Arc<Task>>,
-    task: Arc<Task>,
-}
+
 struct Task {
     fut: Mutex<Option<TopFuture>>,
     id: usize,
+    sender: mpsc::Sender<Arc<Task>>,
 }
 
 fn clone_rw(p: *const ()) -> RawWaker {
-    let data: Arc<Resource> = unsafe { Arc::from_raw(p as *const Resource) };
+    let data: Arc<Task> = unsafe { Arc::from_raw(p as *const Task) };
 
     // make sure increment reference count of the underlying source
     // clone increment ref count, into_raw consume the cloned and escape drop
@@ -112,7 +116,7 @@ fn clone_rw(p: *const ()) -> RawWaker {
     // do not decrement ref count
     forget(data);
 
-    // new RawWaker with data pointer to same resource
+    // new RawWaker with data pointer to same Task
     RawWaker::new(
         p as *const (),
         // the `RawWakerVTable::new` is a magic `const` function can create a object with 'static lifetime
@@ -121,26 +125,26 @@ fn clone_rw(p: *const ()) -> RawWaker {
 }
 
 fn wake_rw(p: *const ()) {
-    let data: Arc<Resource> = unsafe { Arc::from_raw(p as *const Resource) };
+    let data: Arc<Task> = unsafe { Arc::from_raw(p as *const Task) };
     // this can be called from another thread
-    let _mtx = data.task.fut.lock().unwrap();
+    let _mtx = data.fut.lock().unwrap();
     // sending task to executor
-    data.sender.send(data.task.clone()).unwrap();
+    data.sender.send(data.clone()).unwrap();
 }
 
 fn wake_by_ref_rw(p: *const ()) {
-    let data: Arc<Resource> = unsafe { Arc::from_raw(p as *const Resource) };
+    let data: Arc<Task> = unsafe { Arc::from_raw(p as *const Task) };
 
     // this can be called from another thread
     {
-        let _mtx = data.task.fut.lock().unwrap();
+        let _mtx = data.fut.lock().unwrap();
         // sending task to executor
-        data.sender.send(data.task.clone()).unwrap();
+        data.sender.send(data.clone()).unwrap();
     }
     forget(data);
 }
 
 fn drop_rw(p: *const ()) {
-    unsafe { Arc::from_raw(p as *const Resource) };
+    unsafe { Arc::from_raw(p as *const Task) };
     // decrement reference count by auto drop
 }
